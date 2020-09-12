6e7594c2e757d35e6f1e56d12dd490e0b1314d59
Sam <shnachman@gmail.com>
2020-09-12 16:16:42 -0400

    gui init

--- GUI.py
+++ GUI.py
@@ -1,4 +1,27 @@
 import tkinter
 
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = tkinter.Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.label = tkinter
+
+
+        # self.greet_button = tkinter.Button(master, text="Greet", command=self.greet)
+        # self.greet_button.pack()
+        #
+        # self.close_button = tkinter.Button(master, text="Close", command=master.quit)
+        # self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
+
 def main_window():
-    tkinter.Tk()
+    root = tkinter.Tk()
+    my_gui = MyFirstGUI(root)
+    root.mainloop()

d603296ae71b005ed3980ca345f4bebc9253556b
Sam <shnachman@gmail.com>
2020-09-12 17:30:34 -0400

    testing dd

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import hashlib
 import shutil
 import bulk_extractor
-
+import dd
 
 def compute_hash(file):
     m = hashlib.sha256()
@@ -26,6 +26,7 @@ def make_copy(file, output):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # GUI.main_window()
+    dd.from_hardware("/dev/sdb", "image.dd")
     # bulk_extractor.bulk_data_extractor("file.dd", "outputdir/", [])
     bulk_extractor.read_bulk_data("/home/kali/Documents/")
 

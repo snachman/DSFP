e4886fdae6595b0375f2b6b8ac1032775a16b1e6
Sam <shnachman@gmail.com>
2020-09-12 16:49:30 -0400

    added method to test for empty files

--- bulk_extractor.py
+++ bulk_extractor.py
@@ -7,10 +7,15 @@ def bulk_data_extractor(file, outdir, flags):
     # os.system(f"bulk-extractor -o {outdir} {flagString} {file}")
     print(f"bulk-extractor -o {outdir} {flags} {file}")
 
+
 def read_bulk_data(output_location):
     files = (os.listdir(output_location))
     for file in files:
-        fullpath = (output_location+ file)
-        with open(fullpath, "r") as f:
-            print(f.read())
+        fullpath = (output_location + file)
+        if(test_empty_files(fullpath)):
+            with open(fullpath, "r") as f:
+                print(f.read())
 
+def test_empty_files(file):
+    if(os.path.getsize(file) > 0):
+        return True

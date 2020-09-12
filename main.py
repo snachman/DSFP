d3892115d192f307735da305a51d65edb4e69c30
Sam <shnachman@gmail.com>
2020-09-12 09:50:17 -0400

    make_copy method

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 import hashlib
+import shutil
+
 
 def compute_hash(file):
     m = hashlib.sha256()
@@ -9,6 +11,16 @@ def compute_hash(file):
     return(m.hexdigest())
 
 
+def make_copy(file, output):
+    original_hash = compute_hash(file)
+    shutil.copyfile(file, output)
+    copy_hash = compute_hash(output)
+    if original_hash == copy_hash:
+        print("PASS")
+        return True
+    else:
+        print("FAIL")
+        return False
 
 
 # Press the green button in the gutter to run the script.

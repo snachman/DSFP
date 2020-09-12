5304e0f40aee55c45580e218136a2665cecd2bc1
Sam <shnachman@gmail.com>
2020-09-12 17:22:46 -0400

    file rename

--- dc3dd.py
+++
@@ -1,3 +0,0 @@
-
-def from_hardware(hardware_location, output_file):
-    print(f"dc3dd if={hardware_location} of={output_file}")
---
+++ dd.py
@@ -0,0 +1,3 @@
+
+def from_hardware(hardware_location, output_file):
+    print(f"dd if={hardware_location} of={output_file}")

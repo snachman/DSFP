0dfb71c7afc4490147cd23d09fe01efb4ed6150f
Sam <shnachman@gmail.com>
2020-09-13 12:01:02 -0400

    user and case number entering and retrieval

--- GUI.py
+++ GUI.py
@@ -11,29 +11,25 @@ class GUI:
     def set_case_info(self, user, case):
         self.user = user
         self.case_number = case
-        print(user)
 
+    def see_case_info(self):
+        case_info_window = gz.Window(self.app, title="Case Info")
+        investigator_name_label = gz.Text(case_info_window, text=self.user)
+        case_number_label = gz.Text(case_info_window, text=self.case_number)
 
     def main_window(self, app):
-        menubar = gz.MenuBar(self.app, toplevel=["File"], options=[[["New Case", self.popup], ["Exit", app.destroy]]])
-        user_label = gz.Text(self.app, text=self.user)
-        case_label = gz.Text(self.app, text=self.case_number)
-
-
-
-
+        menubar = gz.MenuBar(self.app, toplevel=["File"], options=[[["New Case", self.new_case_popup], ["Case Info", self.see_case_info], ["Exit", app.destroy]]])
         self.app.display()
 
+    def new_case_popup(self):
+        new_case_window = gz.Window(self.app, title="Second window", height=200, width=200)
+        name_label = gz.Text(new_case_window, text="Enter Name:")
+        name_box = gz.TextBox(new_case_window)
+        case_number_label = gz.Text(new_case_window, text="Case Number:")
+        case_number_box = gz.TextBox(new_case_window)
 
-    def popup(self):
-        window = gz.Window(self.app, title="Second window")
-        name_label = gz.Text(window, text="Enter Name:")
-        name_box = gz.TextBox(window)
-        case_number_label = gz.Text(window, text="Case Number:")
-        case_number_box = gz.TextBox(window)
 
-
-        submit_button = gz.PushButton(window, text="Submit", command=lambda: self.set_case_info(name_box.value, 22))
+        submit_button = gz.PushButton(new_case_window, text="Submit", command=lambda: self.set_case_info(name_box.value, str(case_number_box.value)))
 
 
 

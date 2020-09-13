b508ed45efaca3aa411c79d48acc1bd5fc62865a
Sam <shnachman@gmail.com>
2020-09-13 10:22:20 -0400

    basic menu

--- GUI.py
+++ GUI.py
@@ -1,2 +1,15 @@
-import guizero
+import guizero as gz
 
+
+def main_window():
+    app = gz.App(title="DSFP")
+    menubar = gz.MenuBar(app, toplevel=["File"], options=[[["New Case", popup()]]])
+
+
+
+
+    app.display()
+
+
+def popup():
+    print("test")

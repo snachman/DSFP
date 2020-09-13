34992e20af58dd973c782ca8ed9865aa47dcdcf6
Sam <shnachman@gmail.com>
2020-09-13 13:09:53 -0400

    gui testing

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import hashlib
-import shutil
-import bulk_extractor
-import dd
-
-
+import caseloads
+import GUI
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # GUI.main_window()
-    dd.from_hardware("/dev/sdb", "image.dd")
+    p1 = GUI.GUI("", 0)
+    # dd.from_hardware("/dev/sdb", "image.dd")
     # # bulk_extractor.bulk_data_extractor("file.dd", "outputdir/", [])
     # bulk_extractor.read_bulk_data("/home/kali/Documents/")
-
+    # caseloads.create_file()

966b05475bc61f155f70e5c190ee51cd10b6ae48
Sam <shnachman@gmail.com>
2020-09-13 11:34:26 -0400

    basic user and case info

--- GUI.py
+++ GUI.py
@@ -1,15 +1,40 @@
 import guizero as gz
 
 
-def main_window():
-    app = gz.App(title="DSFP")
-    menubar = gz.MenuBar(app, toplevel=["File"], options=[[["New Case", popup()]]])
+class GUI:
+    def __init__(self, user, case_number):
+        self.user = user
+        self.case_number = case_number
+        self.app = gz.App(title="DSFP")
+        self.main_window(self.app)
+
+    def set_case_info(self, user, case):
+        self.user = user
+        self.case_number = case
+        print(user)
+
+
+    def main_window(self, app):
+        menubar = gz.MenuBar(self.app, toplevel=["File"], options=[[["New Case", self.popup], ["Exit", app.destroy]]])
+        user_label = gz.Text(self.app, text=self.user)
+        case_label = gz.Text(self.app, text=self.case_number)
 
 
 
 
-    app.display()
+        self.app.display()
 
 
-def popup():
-    print("test")
+    def popup(self):
+        window = gz.Window(self.app, title="Second window")
+        name_label = gz.Text(window, text="Enter Name:")
+        name_box = gz.TextBox(window)
+        case_number_label = gz.Text(window, text="Case Number:")
+        case_number_box = gz.TextBox(window)
+
+
+        submit_button = gz.PushButton(window, text="Submit", command=lambda: self.set_case_info(name_box.value, 22))
+
+
+
+

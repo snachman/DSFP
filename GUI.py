43d7259e598d464c4e7d5d6015cde8459f3846bb
Sam <shnachman@gmail.com>
2020-09-13 22:12:23 -0400

    menubar problems

--- GUI.py
+++ GUI.py
@@ -7,6 +7,7 @@ class GUI:
         self.case_number = 0
         self.app = gz.App(title="DSFP", height=200, width=200)
         self.main_window(self.app)
+        self.menubar = gz.MenuBar(toplevel=["File"], options=[[["New Case", self.new_case_popup], ["Case Info", self.see_case_info], ["Exit", app.destroy]]])
 
     def set_case_info(self, user, case):
         self.user = user
@@ -19,7 +20,6 @@ class GUI:
 
 
     def main_window(self, app):
-        # menubar = gz.MenuBar(self.app, toplevel=["File"], options=[[["New Case", self.new_case_popup], ["Case Info", self.see_case_info], ["Exit", app.destroy]]])
         name_label = gz.Text(self.app, text="Enter Name:")
         name_box = gz.TextBox(self.app)
         case_number_label = gz.Text(self.app, text="Case Number:")
@@ -30,11 +30,11 @@ class GUI:
     def new_case_popup(self, user, caseno):
         # self.app.hide()
 
-        new_case_window = gz.Window(self.app, title="Second window", height=600, width=600, menubar=menubar)
-        menubar = gz.MenuBar(new_case_window, toplevel=["File"], options=[[["Case Info", self.see_case_info], ["Exit", self.app.destroy]]])
-
+        new_case_window = gz.Window(self.app, title="Second window", height=600, width=600, menubar=self.menubar)
         original_label = gz.Text(new_case_window, text="Original:")
         original_text_box = gz.TextBox(new_case_window)
+        output_label = gz.Text(new_case_window, text="Output:")
+        output_text_box = gz.TextBox(new_case_window)
 
 
 
